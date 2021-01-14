643fd076759b4301df1dba94067b850faa323c21
Hussein <tejanahmedhu@gmail.com>
2021-01-14 12:31:31 +0000

    Writing Strings

--- my_app.py
+++ my_app.py
@@ -1,21 +1,5 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+from docx import Document
 
-    def walk(self):
-        print(self.name + ' is walking ...')
+document = Document()
 
-    def speak(self):
-        print('Hello my name is ' + self.name + ' and I am ' + str(self.age) +' years old.')
-
-mohamed = Person('Mohamed', 32)
-mariatou = Person('Mariatou', 29)
-
-print(mohamed.name + ' ' + str(mohamed.age))
-mohamed.speak()
-mohamed.walk()
-
-print(mariatou.name + ' ' + str(mariatou.age))
-mariatou.walk()
-mariatou.speak()
+document.save('cv.docx')
